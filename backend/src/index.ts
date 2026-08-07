68d8d1f26e90c4950cd80b49eba607ba1fcd7431
Big J <jonalmarez@JBs-MacBook-Pro.local>
2026-08-07 11:29:30 +0800

    chore: set up typescript and main endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,13 @@
-const message:string = "Hello World";
+import express from "express";
+import type { Request, Response } from "express";
 
-console.log(message);
+const app = express();
+const PORT: number = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World! From TypeScript with Express");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
